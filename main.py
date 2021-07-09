83ed6f3557f4d01faf972ad33b570803e71a28ff
Mark Theiding <mark.theiding@gmail.com>
2021-07-09 11:47:28 -0700

    Add calc functions

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import json
 with open("Configuration.json","r") as f:
     x = json.load(f)
 print ("dictionary loaded from JSON file" + str(x))
+
+KEY_SAVINGS = 'savings'
+
+def calcSavings(current, previous):
+    if (previous == {}):
+        current[KEY_SAVINGS] = 5 #XXX load from config 
+    else:
+        current[KEY_SAVINGS] = previous[KEY_SAVINGS] + 1
+        
+def calcYear(current, previous):
+    calcSavings(current, previous)
+    
+    
