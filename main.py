acc678da7fafdd899f36d2755759e369c6b13e90
Mark Theiding <mark.theiding@gmail.com>
2021-07-09 12:19:02 -0700

    Add calcExpenses and calcIncomeWork

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import json
 
 KEY_YEAR = 'year'
 KEY_SAVINGS = 'savings'
+KEY_EXPENSES = 'expenses'
+KEY_INCOME_WORK_HUSBAND = 'incomeworkhusband'
+KEY_INCOME_WORK_WIFE = 'incomeworkwife'
+
 
 def calcSavings(current, previous):
     if (previous == None):
@@ -9,8 +13,26 @@ def calcSavings(current, previous):
     else:
         current[KEY_SAVINGS] = previous[KEY_SAVINGS] + 1
 
+def calcExpenses(current, previous):
+    if (previous == {}):
+        current[KEY_EXPENSES] = -1 #XXX load from config 
+    else:
+        current[KEY_EXPENSES] = previous[KEY_EXPENSES] * 1.04 #XXX load inflation from config
+
+def calcIncomeWork(current, previous):
+    if current[KEY_YEAR] >= 2021 and current[KEY_YEAR] <= 2023: #XXX load from and to husband employment from config
+        current[KEY_INCOME_WORK_HUSBAND] = 100000 #XXX load from config
+    else:
+        current[KEY_INCOME_WORK_HUSBAND] = 0
+    if current[KEY_YEAR] >= 2021 and current[KEY_YEAR] <= 2023: #XXX load from and to wife employment from config
+        current[KEY_INCOME_WORK_WIFE] = 50000 #XXX load from config
+    else:
+        current[KEY_INCOME_WORK_WIFE] = 0
+        
 def calcYear(current, previous):
     calcSavings(current, previous)
+    calcExpenses(current, previous)
+    calcIncomeWork(current, previous)
 
 #------------------ Main loop
 
@@ -24,4 +46,3 @@ for year in range(2021, 2071):
     calcYear( current, previous)
     years.append(current)
     previous = current
-
